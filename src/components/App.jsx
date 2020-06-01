eb8ba82affdf68c2d589257feab5ba45eac1e7c6
NayanaRaghavaShet5 <51402931+NayanaRaghavaShet5@users.noreply.github.com>
2020-06-02 00:13:31 +0530

    c
    c

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+
+const [title, setTitle] = useState("Hello");
+const [isOver, setOver] = useState(false);
+function changeName(){
+  setTitle("Submitted"); 
+}
+
+function changeOnOver(){
+ setOver(true);
+  }
+
+  function changeOnOut(){
+    setOver(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{title}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick = { changeName } style = {{backgroundColor : isOver ? "black" : "white"}}
+     onMouseOver= { changeOnOver }
+     onMouseOut = { changeOnOut }>Submit</button>
     </div>
   );
 }
